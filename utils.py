5972abb83f88e5776d888f9b2fa7aacbbc9d9fac
wiktoria.wiatrowska <wiatrowka@student.agh.edu.pl>
2024-11-13 21:11:03 +0100

    Add read_file and send_to_chatbot functions to utils.py file.

---
+++ utils.py
@@ -0,0 +1,27 @@
+import openai
+import logging
+
+def read_file(filename):
+    try:
+        file_with_text = open(filename, 'r')
+    except FileNotFoundError:
+        logging.error(f"Cannot find file: {filename}")
+        return None
+    else:
+        logging.info(f"Read file {filename}")
+        return file_with_text.read()
+
+
+def send_to_chatbot(key, prompt_file, article_file):
+    """The function is supposed to combine prompt and given article.
+    Then return chatbot's answer in form of .html file using openai.Completion.create() function."""
+
+    prompt_text = read_file(prompt_file)
+    article_text = read_file(article_file)
+
+    full_prompt = prompt_text + "\n\n" + article_text
+
+
+
+    return full_prompt
+
